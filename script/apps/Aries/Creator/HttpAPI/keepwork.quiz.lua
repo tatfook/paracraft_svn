9850be4e309bf29505dd5f9fd16dc556310ce69c
Administrator <admin@example.com>
2020-07-07 09:08:01 +0800

    svn_sync

---
+++ script/apps/Aries/Creator/HttpAPI/keepwork.quiz.lua
@@ -0,0 +1,37 @@
+--[[
+Title: keepwork.quiz
+Author(s): dreamanddead
+Date: 2020/7/3
+Desc:  
+Use Lib:
+-------------------------------------------------------
+NPL.load("(gl)script/apps/Aries/Creator/HttpAPI/keepwork.quiz.lua");
+]]
+local HttpWrapper = NPL.load("(gl)script/apps/Aries/Creator/HttpAPI/HttpWrapper.lua");
+
+
+--http://yapi.kp-para.cn/project/158/interface/api/2337
+-- get
+HttpWrapper.Create("keepwork.quiz.submit.b.score", "%MAIN%/online-quiz/v0/activityUserScore/score", "POST", true, nil,
+-- PreProcessor
+function(self, inputParams, callbackFunc, option)
+   return HttpWrapper.default_prepFunc(self, inputParams, callbackFunc, option, "keepwork.quiz.submit.b.score.post")
+end,
+-- Post Processor
+function(self, err, msg, data)
+    return HttpWrapper.default_postFunc(self, err, msg, data, "keepwork.quiz.submit.b.score.post", callbackFunc); 
+end
+)
+
+--http://yapi.kp-para.cn/project/158/interface/api/2442
+-- get
+HttpWrapper.Create("keepwork.quiz.checkavailable", "%MAIN%/online-quiz/v0/activity/checkAvailable", "GET", true, nil,
+-- PreProcessor
+function(self, inputParams, callbackFunc, option)
+   return HttpWrapper.default_prepFunc(self, inputParams, callbackFunc, option, "keepwork.quiz.checkavailable.get")
+end,
+-- Post Processor
+function(self, err, msg, data)
+    return HttpWrapper.default_postFunc(self, err, msg, data, "keepwork.quiz.checkavailable.get", callbackFunc); 
+end
+)
