983178f2b4a59a65cf59f87661bb82706e2deac4
Administrator <admin@example.com>
2020-07-14 09:08:01 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Network/Admin/ClassManager/ChatRoomPage.lua
+++ script/apps/Aries/Creator/Game/Network/Admin/ClassManager/ChatRoomPage.lua
@@ -1,4 +1,4 @@
---[[
+﻿--[[
 Title: Class List 
 Author(s): Chenjinxian
 Date: 2020/7/6
@@ -9,6 +9,7 @@ local ChatRoomPage = NPL.load("(gl)script/apps/Aries/Creator/Game/Network/Admin/
 ChatRoomPage.ShowPage()
 -------------------------------------------------------
 ]]
+local TeacherPanel = NPL.load("(gl)script/apps/Aries/Creator/Game/Network/Admin/ClassManager/TeacherPanel.lua");
 local ChatRoomPage = NPL.export()
 
 local page;
@@ -26,13 +27,14 @@ function ChatRoomPage.ShowPage()
 		style = CommonCtrl.WindowFrame.ContainerStyle,
 		allowDrag = true,
 		enable_esc_key = true,
+		click_through = false, 
 		app_key = MyCompany.Aries.Creator.Game.Desktop.App.app_key, 
 		directPosition = true,
 		align = "_ct",
-		x = -730 / 2,
-		y = -520 / 2,
-		width = 730,
-		height = 520,
+		x = -750 / 2,
+		y = -533 / 2,
+		width = 750,
+		height = 533,
 	};
 	System.App.Commands.Call("File.MCMLWindowFrame", params);
 end
@@ -41,14 +43,27 @@ function ChatRoomPage.OnClose()
 	page:CloseWindow();
 end
 
-function ChatRoomPage.GetClassList()
-	local classes = {}
-	return classes;
+function ChatRoomPage.GetClassName()
+	--return TeacherPanel.CurrentClassName;
+	return L"编程1班";
 end
 
-function ChatRoomPage.OnSelectClass()
+function ChatRoomPage.GetClassPeoples()
+	return L"班级成员 10/20";
 end
 
-function ChatRoomPage.OnOK()
-	page:CloseWindow();
+function ChatRoomPage.InviteAll()
+end
+
+function ChatRoomPage.ClassItems()
+	local items = {{name="张晓", teacher=true, online=true},
+		{name="张飞", teacher=false, online=false},
+		{name="赵云", teacher=false, online=true}};
+	return items;
+end
+
+function ChatRoomPage.ChatItems()
+end
+
+function ChatRoomPage.SendMessage()
 end
