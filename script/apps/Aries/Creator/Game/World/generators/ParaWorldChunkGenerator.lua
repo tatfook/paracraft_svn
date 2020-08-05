56b21ce97b6d43edd789b681c3a8ed57e8836c29
Administrator <admin@example.com>
2020-08-05 10:38:02 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/World/generators/ParaWorldChunkGenerator.lua
+++ script/apps/Aries/Creator/Game/World/generators/ParaWorldChunkGenerator.lua
@@ -1,12 +1,12 @@
 --[[
-Title: SocialGridChunkGenerator
+Title: ParaWorldChunkGenerator
 Author(s): LiXizhi
 Date: 2013/8/27, refactored 2015.11.17
 Desc: A flat world generator with multiple layers at custom level.
 -----------------------------------------------
-NPL.load("(gl)script/apps/Aries/Creator/Game/World/generators/SocialGridChunkGenerator.lua");
-local SocialGridChunkGenerator = commonlib.gettable("MyCompany.Aries.Game.World.Generators.SocialGridChunkGenerator");
-ChunkGenerators:Register("flat", SocialGridChunkGenerator);
+NPL.load("(gl)script/apps/Aries/Creator/Game/World/generators/ParaWorldChunkGenerator.lua");
+local ParaWorldChunkGenerator = commonlib.gettable("MyCompany.Aries.Game.World.Generators.ParaWorldChunkGenerator");
+ChunkGenerators:Register("paraworld", ParaWorldChunkGenerator);
 -----------------------------------------------
 ]]
 NPL.load("(gl)script/apps/Aries/Creator/Game/World/ChunkGenerator.lua");
@@ -14,42 +14,40 @@ local BlockEngine = commonlib.gettable("MyCompany.Aries.Game.BlockEngine");
 local block_types = commonlib.gettable("MyCompany.Aries.Game.block_types")
 local names = commonlib.gettable("MyCompany.Aries.Game.block_types.names");
 
-local SocialGridChunkGenerator = commonlib.inherit(commonlib.gettable("MyCompany.Aries.Game.World.ChunkGenerator"), commonlib.gettable("MyCompany.Aries.Game.World.Generators.SocialGridChunkGenerator"))
+local ParaWorldChunkGenerator = commonlib.inherit(commonlib.gettable("MyCompany.Aries.Game.World.ChunkGenerator"), commonlib.gettable("MyCompany.Aries.Game.World.Generators.ParaWorldChunkGenerator"))
 
-function SocialGridChunkGenerator:ctor()
+function ParaWorldChunkGenerator:ctor()
 end
 
 -- @param world: WorldManager, if nil, it means a local generator. 
 -- @param seed: a number
-function SocialGridChunkGenerator:Init(world, seed)
-	SocialGridChunkGenerator._super.Init(self, world, seed);
+function ParaWorldChunkGenerator:Init(world, seed)
+	ParaWorldChunkGenerator._super.Init(self, world, seed);
 	return self;
 end
 
-function SocialGridChunkGenerator:OnExit()
-	SocialGridChunkGenerator._super.OnExit(self);
+function ParaWorldChunkGenerator:OnExit()
+	ParaWorldChunkGenerator._super.OnExit(self);
 end
 
 -- get params for generating flat terrain
 -- one can modify its properties before running custom chunk generator. 
-function SocialGridChunkGenerator:GetFlatLayers()
+function ParaWorldChunkGenerator:GetFlatLayers()
 	if(self.flat_layers == nil) then
 		self.flat_layers = {
-			{y = 0, block_id = names.Bedrock},
+			{y = 9, block_id = names.Bedrock},
 			{block_id = names.underground_default},
-			{block_id = names.underground_default},
-			{block_id = names.underground_shell},
 		};
 	end
 	return self.flat_layers;
 end
 
-function SocialGridChunkGenerator:SetFlatLayers(layers)
+function ParaWorldChunkGenerator:SetFlatLayers(layers)
 	self.flat_layers = layers;
 end
 
 -- generate flat terrain
-function SocialGridChunkGenerator:GenerateFlat(c, x, z)
+function ParaWorldChunkGenerator:GenerateFlat(c, x, z)
 	local layers = self:GetFlatLayers();
 			
 	local by = layers[1].y;
@@ -68,24 +66,24 @@ end
 
 -- protected virtual funtion:
 -- generate chunk for the entire chunk column at x, z
-function SocialGridChunkGenerator:GenerateChunkImp(chunk, x, z, external)
+function ParaWorldChunkGenerator:GenerateChunkImp(chunk, x, z, external)
 	self:GenerateFlat(chunk, x, z);
 end
 
 -- virtual function: this is run in worker thread. It should only use data in the provided chunk.
 -- if this function returns false, we will use GenerateChunkImp() instead. 
-function SocialGridChunkGenerator:GenerateChunkAsyncImp(chunk, x, z)
+function ParaWorldChunkGenerator:GenerateChunkAsyncImp(chunk, x, z)
 	return false
 end
 
-function SocialGridChunkGenerator:IsSupportAsyncMode()
+function ParaWorldChunkGenerator:IsSupportAsyncMode()
 	return false;
 end
 
 -- virtual function: get the class address for sending to worker thread. 
-function SocialGridChunkGenerator:GetClassAddress()
+function ParaWorldChunkGenerator:GetClassAddress()
 	return {
-		filename="script/apps/Aries/Creator/Game/World/generators/SocialGridChunkGenerator.lua", 
-		classpath="MyCompany.Aries.Game.World.Generators.SocialGridChunkGenerator"
+		filename="script/apps/Aries/Creator/Game/World/generators/ParaWorldChunkGenerator.lua", 
+		classpath="MyCompany.Aries.Game.World.Generators.ParaWorldChunkGenerator"
 	};
 end
