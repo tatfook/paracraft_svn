20b8381f030563af3e5ffe2bd62537f9982064fd
Administrator <admin@example.com>
2020-08-17 15:18:02 +0800

    svn_sync

---
+++ script/apps/Aries/Creator/Game/Tasks/Dock/DockTipPage.lua
@@ -0,0 +1,10 @@
+--[[
+Title: DockTipPage
+Author(s): leio
+Date: 2020/8/17
+Desc:  
+Use Lib:
+-------------------------------------------------------
+local DockTipPage = NPL.load("(gl)script/apps/Aries/Creator/Game/Tasks/Dock/DockTipPage.lua");
+--]]
+local DockTipPage = NPL.export();
