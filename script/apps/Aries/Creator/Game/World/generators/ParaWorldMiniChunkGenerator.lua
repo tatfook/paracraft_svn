cdc7797fb92c8c8306153b4492cc3c69b6abe442
Administrator <admin@example.com>
2020-09-03 11:08:01 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/World/generators/ParaWorldMiniChunkGenerator.lua
+++ script/apps/Aries/Creator/Game/World/generators/ParaWorldMiniChunkGenerator.lua
@@ -107,6 +107,9 @@ function ParaWorldMiniChunkGenerator:OnLoadWorld()
 		self.count = count;
 		self:ShowBlockTip()
 	end
+	GameLogic.RunCommand("/speedscale 2");
+	GameLogic.RunCommand("/viewbobbing off");
+	
 	self.timer = self.timer or commonlib.Timer:new({callbackFunc = function(timer)
 		self:OnTimer()
 	end})
