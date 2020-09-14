cf34747ca4ed99cdb54958a8650c1eacf715efe1
Administrator <admin@example.com>
2020-09-14 10:38:03 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/World/generators/ParaWorldMiniChunkGenerator.lua
+++ script/apps/Aries/Creator/Game/World/generators/ParaWorldMiniChunkGenerator.lua
@@ -15,6 +15,7 @@ local EntityManager = commonlib.gettable("MyCompany.Aries.Game.EntityManager");
 local BlockEngine = commonlib.gettable("MyCompany.Aries.Game.BlockEngine");
 local block_types = commonlib.gettable("MyCompany.Aries.Game.block_types")
 local names = commonlib.gettable("MyCompany.Aries.Game.block_types.names");
+local WorldCommon = commonlib.gettable("MyCompany.Aries.Creator.WorldCommon")
 
 local ParaWorldMiniChunkGenerator = commonlib.inherit(commonlib.gettable("MyCompany.Aries.Game.World.ChunkGenerator"), commonlib.gettable("MyCompany.Aries.Game.World.Generators.ParaWorldMiniChunkGenerator"))
 local defaultFilename = "miniworld.template.xml"
@@ -196,37 +197,41 @@ function ParaWorldMiniChunkGenerator:OnSaveWorld()
 		params = params,
 		blocks = blocks})
 	task:Run();
-	
-	local function uploadMiniWorld(projectId)
-		keepwork.world.joined_list({}, function(err, msg, data)
-			if (data and type(data) == "table") then
-				for i = 1, #data do
-					local world = data[i];
-					if (world.projectId == projectId) then
-						keepwork.miniworld.upload({projectId = projectId, name = world.worldName, type="main", commitId = world.commitId}, function(err, msg, data)
-							if (err == 200) then
-								_guihelper.MessageBox(L"上传成功！");
-							end
-						end);
-						break;
-					end
-				end
-			end
-		end);
-	end
 
-	_guihelper.MessageBox(L"世界已保存，是否要上传迷你世界？", function(res)
-		if(res and res == _guihelper.DialogResult.Yes)then
-			GameLogic.GetFilters():apply_filters("SaveWorldPage.ShowSharePage", true, function(res)
-				if (res) then
-					local currentWorld = Mod.WorldShare.Store:Get('world/currentWorld');
-					if (currentWorld and currentWorld.kpProjectId) then
-						uploadMiniWorld(tonumber(currentWorld.kpProjectId));
+	local myHomeWorldName = string.format(L"%s的家园", System.User.keepworkUsername);
+	local currentWorldName = WorldCommon.GetWorldTag("name");
+	if (myHomeWorldName == currentWorldName and WorldCommon.GetWorldTag("world_generator") == "paraworldMini") then
+		local function uploadMiniWorld(projectId)
+			keepwork.world.joined_list({}, function(err, msg, data)
+				if (data and type(data) == "table") then
+					for i = 1, #data do
+						local world = data[i];
+						if (world.projectId == projectId) then
+							keepwork.miniworld.upload({projectId = projectId, name = world.worldName, type="main", commitId = world.commitId}, function(err, msg, data)
+								if (err == 200) then
+									_guihelper.MessageBox(L"上传成功！");
+								end
+							end);
+							break;
+						end
 					end
 				end
 			end);
 		end
-	end, _guihelper.MessageBoxButtons.YesNo);
+
+		_guihelper.MessageBox(L"世界已保存，是否要上传迷你世界？", function(res)
+			if(res and res == _guihelper.DialogResult.Yes)then
+				GameLogic.GetFilters():apply_filters("SaveWorldPage.ShowSharePage", true, function(res)
+					if (res) then
+						local currentWorld = Mod.WorldShare.Store:Get('world/currentWorld');
+						if (currentWorld and currentWorld.kpProjectId) then
+							uploadMiniWorld(tonumber(currentWorld.kpProjectId));
+						end
+					end
+				end);
+			end
+		end, _guihelper.MessageBoxButtons.YesNo);
+	end
 end
 
 -- get params for generating flat terrain
