314edb2e9e30fe3608f84d007a4953e91ad38a6d
Administrator <admin@example.com>
2020-09-17 17:38:02 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Login/ParaWorldLessons.lua
+++ script/apps/Aries/Creator/Game/Login/ParaWorldLessons.lua
@@ -554,13 +554,13 @@ function ParaWorldLessons.OnWorldLoaded()
 
 	if(lesson) then
 		local nQuizCount = lesson:GetQuizCount();
-		local text = L"点击图标打开课程学习页面";
-		if(nQuizCount>0) then
-			text = text.."<br/>"..lesson:GetSummaryMCML();
-		end
-		text = text.."<br/><div style='color:#cc3300'>"..(L"学习完毕可领取奖励~").."</div>";
+		-- local text = L"点击图标打开课程学习页面";
+		-- if(nQuizCount>0) then
+		-- 	text = text.."<br/>"..lesson:GetSummaryMCML();
+		-- end
+		-- text = text.."<br/><div style='color:#cc3300'>"..(L"学习完毕可领取奖励~").."</div>";
 
-		TeacherAgent:AddTaskButton("OpenLesson", "Texture/3DMapSystem/AppIcons/png/Intro_64.png", ParaWorldLessons.OpenCurrentLessonUrl, nQuizCount, 100, text)
+		TeacherAgent:AddTaskButton("OpenLesson", "Texture/3DMapSystem/AppIcons/png/Intro_64.png", ParaWorldLessons.OpenCurrentLessonUrl, nQuizCount, 100, "")
 		TeacherAgent:SetEnabled(true);
 	end
 end
