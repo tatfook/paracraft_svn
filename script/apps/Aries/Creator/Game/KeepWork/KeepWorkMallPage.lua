53f18b64685e5e8dd921d7f4d6a185caf41b22d3
Administrator <admin@example.com>
2020-11-13 09:53:01 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/KeepWork/KeepWorkMallPage.lua
+++ script/apps/Aries/Creator/Game/KeepWork/KeepWorkMallPage.lua
@@ -130,7 +130,7 @@ function KeepWorkMallPage.ShowView()
 				style = CommonCtrl.WindowFrame.ContainerStyle,
 				allowDrag = true,
 				enable_esc_key = true,
-				zorder = 1,
+				zorder = 0,
 				app_key = MyCompany.Aries.Creator.Game.Desktop.App.app_key, 
 				directPosition = true,
 					align = "_ct",
