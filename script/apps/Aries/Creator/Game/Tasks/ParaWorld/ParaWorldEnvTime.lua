a27c783546bcf62c7aecbffc8580748f18a69a67
Administrator <admin@example.com>
2020-11-26 14:28:01 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Tasks/ParaWorld/ParaWorldEnvTime.lua
+++ script/apps/Aries/Creator/Game/Tasks/ParaWorld/ParaWorldEnvTime.lua
@@ -10,41 +10,35 @@ ParaWorldEnvTime.ShowPage();
 -------------------------------------------------------
 ]]
 local CommandManager = commonlib.gettable("MyCompany.Aries.Game.CommandManager");
+NPL.load("(gl)script/mobile/paracraft/Areas/SettingPage.lua");
+local SettingPage = commonlib.gettable("ParaCraft.Mobile.Desktop.SettingPage");
 local ParaWorldEnvTime = NPL.export();
 
-local isEnvTimeShow = false;
+local currentTime;
 local page;
 function ParaWorldEnvTime.OnInit()
 	page = document:GetPageCtrl();
 end
 
 function ParaWorldEnvTime.ShowPage()
-	if (not isEnvTimeShow) then
-		isEnvTimeShow = true;
-		local params = {
-			url = "script/apps/Aries/Creator/Game/Tasks/ParaWorld/ParaWorldEnvTime.html",
-			name = "ParaWorldEnvTime.ShowPage", 
-			isShowTitleBar = false,
-			DestroyOnClose = true,
-			style = CommonCtrl.WindowFrame.ContainerStyle,
-			allowDrag = false,
-			enable_esc_key = true,
-			app_key = MyCompany.Aries.Creator.Game.Desktop.App.app_key, 
-			directPosition = true,
-			align = "_rt",
-			x = -260,
-			y = 256,
-			width = 220,
-			height = 80,
-		};
-		System.App.Commands.Call("File.MCMLWindowFrame", params);
-
-		params._page.OnClose = function()
-			isEnvTimeShow = false;
-		end
-	else
-		ParaWorldEnvTime.OnClose();
-	end
+	--currentTime = currentTime or SettingPage.GetTimeOfDayStd();
+	local params = {
+		url = "script/apps/Aries/Creator/Game/Tasks/ParaWorld/ParaWorldEnvTime.html",
+		name = "ParaWorldEnvTime.ShowPage", 
+		isShowTitleBar = false,
+		DestroyOnClose = true,
+		style = CommonCtrl.WindowFrame.ContainerStyle,
+		allowDrag = true,
+		enable_esc_key = true,
+		app_key = MyCompany.Aries.Creator.Game.Desktop.App.app_key, 
+		directPosition = true,
+		align = "_ct",
+		x = -400 / 2,
+		y = -160 / 2,
+		width = 400,
+		height = 160,
+	};
+	System.App.Commands.Call("File.MCMLWindowFrame", params);
 end
 
 function ParaWorldEnvTime.OnClose()
@@ -53,9 +47,20 @@ function ParaWorldEnvTime.OnClose()
 	end
 end
 
+function ParaWorldEnvTime.SetDefault()
+	currentTime = SettingPage.GetTimeOfDayStd();
+	local time = (currentTime/1000-0.5)*2;
+	time = tostring(time);
+	CommandManager:RunCommand("time", time);
+	if (page) then
+		page:Refresh(0);
+	end
+end
+
 function ParaWorldEnvTime.OnTimeSliderChanged(value)
 	if (value) then
-		local time=(value/1000-0.5)*2;
+		--currentTime = value;
+		local time = (value/1000-0.5)*2;
 		time = tostring(time);
 		CommandManager:RunCommand("time", time);
 	end	
