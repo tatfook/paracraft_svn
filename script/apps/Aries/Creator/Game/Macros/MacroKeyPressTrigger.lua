754f3f8c26da652a2d033d9bee4aeca388e2ac3f
Administrator <admin@example.com>
2021-01-04 12:48:01 +0800

    svn_sync

---
+++ script/apps/Aries/Creator/Game/Macros/MacroKeyPressTrigger.lua
@@ -0,0 +1,27 @@
+--[[
+Title: Macro Button Click Trigger
+Author(s): LiXizhi
+Date: 2021/1/4
+Desc: a trigger for key(s) press. 
+
+Use Lib:
+-------------------------------------------------------
+NPL.load("(gl)script/apps/Aries/Creator/Game/Common/Macro.lua");
+local Macro = commonlib.gettable("MyCompany.Aries.Game.Macro");
+-------------------------------------------------------
+]]
+-------------------------------------
+-- single Macro base
+-------------------------------------
+local Macros = commonlib.gettable("MyCompany.Aries.Game.GameLogic.Macros")
+
+--@param keyName1: key name1
+--@param keyName2: key name2
+function Macros.KeyPressTrigger(keyName1, keyName2)
+
+end
+
+
+
+
+
