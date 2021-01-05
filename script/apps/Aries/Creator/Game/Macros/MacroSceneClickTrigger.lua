9cf40535793692b19f86909f58e95e137c09ce22
Administrator <admin@example.com>
2021-01-05 16:08:01 +0800

    svn_sync

---
+++ script/apps/Aries/Creator/Game/Macros/MacroSceneClickTrigger.lua
@@ -0,0 +1,27 @@
+--[[
+Title: Macro Button Click Trigger
+Author(s): LiXizhi
+Date: 2021/1/4
+Desc: a trigger for the clicking of a named button in GUI. 
+
+Use Lib:
+-------------------------------------------------------
+NPL.load("(gl)script/apps/Aries/Creator/Game/Common/Macro.lua");
+local Macro = commonlib.gettable("MyCompany.Aries.Game.Macro");
+-------------------------------------------------------
+]]
+-------------------------------------
+-- single Macro base
+-------------------------------------
+local Macros = commonlib.gettable("MyCompany.Aries.Game.GameLogic.Macros")
+
+--@param btnName: button name
+--@param mouse_button: "left", "right", default to "left"
+function Macros.ButtonClickTrigger(btnName, mouse_button)
+
+end
+
+
+
+
+
