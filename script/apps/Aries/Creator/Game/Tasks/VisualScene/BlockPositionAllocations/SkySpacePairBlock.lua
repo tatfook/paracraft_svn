17d215429391cf29b912a1caa040a2366e02db71
Administrator <admin@example.com>
2021-01-08 14:32:06 +0800

    svn_sync

---
+++ script/apps/Aries/Creator/Game/Tasks/VisualScene/BlockPositionAllocations/SkySpacePairBlock.lua
@@ -0,0 +1,15 @@
+--[[
+Title: SkySpacePairBlock
+Author(s): leio
+Date: 2021/1/7
+Desc: 
+use the lib:
+------------------------------------------------------------
+local SkySpace = NPL.load("(gl)script/apps/Aries/Creator/Game/Tasks/VisualScene/BlockPositionAllocations/SkySpacePairBlock.lua");
+------------------------------------------------------------
+--]]
+
+local SkySpacePairBlock = commonlib.inherit(nil, NPL.export());
+
+function SkySpacePairBlock:ctor()
+end
