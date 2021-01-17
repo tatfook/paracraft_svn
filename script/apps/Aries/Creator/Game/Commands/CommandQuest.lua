dbc7cf1cb6fdd3298d498be32e2a94444b6e5c04
Administrator <admin@example.com>
2021-01-17 20:52:02 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Commands/CommandQuest.lua
+++ script/apps/Aries/Creator/Game/Commands/CommandQuest.lua
@@ -15,12 +15,13 @@ local Commands = commonlib.gettable("MyCompany.Aries.Game.Commands");
 
 Commands["macro"] = {
 	name="macro", 
-	quick_ref="/macro [record|play|stop] [-i|interactive] [-r|relative] [-a|autoplay] [-nohelp]", 
+	quick_ref="/macro [record|play|stop] [-i|interactive] [-r|relative] [-a|autoplay] [-nohelp] [-1x|2x|4x]", 
 	desc=[[record user actions to clipboard and then playback from current clipboard. 
 @param i|interactive: we will automatically insert trigger macros
 @param r|relative: play macros relative to current player's block position. 
 @param a|autoplay: auto play mode on
 @param nohelp: play without help text
+@param 1x|2x|4x: play speed, default to 1x. 
 e.g.
 /macro    toggle recording mode
 /macro record -i
@@ -29,6 +30,7 @@ e.g.
 /macro play -r
 /macro play -r -autoplay
 /macro play -r -nohelp
+/macro play -r -a -4x
 /macro stop
 ]], 
 	handler = function(cmd_name, cmd_text, cmd_params, fromEntity)
@@ -40,6 +42,7 @@ e.g.
 		local isRelative = false;
 		local isAutoPlay = false;
 		local nHelpLevel = 1;
+		local playSpeed = 1;
 		if(options) then
 			if(options.i or options.interactive) then
 				isInteractive = true;
@@ -52,6 +55,13 @@ e.g.
 			end
 			if(options.nohelp) then
 				nHelpLevel = 0;
+			end
+			if(options["1x"]) then
+				playSpeed = 1;
+			elseif(options["2x"]) then
+				playSpeed = 2;
+			elseif(options["4x"]) then
+				playSpeed = 4;
 			end
 		end
 		
@@ -75,6 +85,8 @@ e.g.
 			GameLogic.Macros.SetPlayOrigin(x, y, z)
 			GameLogic.Macros.SetAutoPlay(isAutoPlay);
 			GameLogic.Macros.SetHelpLevel(nHelpLevel);
+			GameLogic.Macros.SetPlaySpeed(playSpeed);
+			
 			GameLogic.Macros:Play();
 		elseif(not name or name == "") then
 			if(GameLogic.Macros:IsRecording()) then
