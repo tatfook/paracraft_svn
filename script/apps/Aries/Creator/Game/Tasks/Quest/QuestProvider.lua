e2470f39fd8114e2262cb768c941fb63e367bd06
Administrator <admin@example.com>
2021-01-24 22:12:03 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Tasks/Quest/QuestProvider.lua
+++ script/apps/Aries/Creator/Game/Tasks/Quest/QuestProvider.lua
@@ -542,7 +542,7 @@ function QuestProvider:UpdateServerTime()
     },function(err, msg, data)
         if(err == 200)then
             self.server_time_stamp = commonlib.timehelp.GetTimeStampByDateTime(data.now)
-            local time = System.options.isDevMode and 3000 or 30000
+            local time = System.options.isDevMode and 3000 or 90000
             commonlib.TimerManager.SetTimeout(function()  
                 self:UpdateServerTime()
             end, time)
