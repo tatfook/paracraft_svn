6d2a59cacd3d3420ae0ec473f95b49bfe2103852
Administrator <admin@example.com>
2021-03-08 20:32:02 +0800

    svn_sync

--- script/apps/Aries/Creator/Game/Agent/AgentWorld.lua
+++ script/apps/Aries/Creator/Game/Agent/AgentWorld.lua
@@ -9,11 +9,14 @@ use the lib:
 ------------------------------------------------------------
 NPL.load("(gl)script/apps/Aries/Creator/Game/Agent/AgentWorld.lua");
 local AgentWorld = commonlib.gettable("MyCompany.Aries.Game.Agent.AgentWorld");
-local world = AgentWorld:new():Init();
+local world = AgentWorld:new():Init("Mod/Agents/MacroPlatform.xml");
 world:Run()
 -------------------------------------------------------
 ]]
+local EntityCode = commonlib.gettable("MyCompany.Aries.Game.EntityManager.EntityCode")
+local BlockEngine = commonlib.gettable("MyCompany.Aries.Game.BlockEngine")
 local EntityManager = commonlib.gettable("MyCompany.Aries.Game.EntityManager");
+local block_types = commonlib.gettable("MyCompany.Aries.Game.block_types")
 local GameLogic = commonlib.gettable("MyCompany.Aries.Game.GameLogic")
 
 local AgentWorld = commonlib.inherit(commonlib.gettable("System.Core.ToolBase"), commonlib.gettable("MyCompany.Aries.Game.Agent.AgentWorld"));
@@ -25,17 +28,58 @@ function AgentWorld:ctor()
 	self.codeblocks = {};
 end
 
-function AgentWorld:Init()
+function AgentWorld:Init(filename)
 	GameLogic:Connect("WorldUnloaded", AgentWorld, AgentWorld.OnWorldUnload, "UniqueConnection");
 
+	if(filename) then
+		self:LoadFromAgentFile(filename);
+	end
 	return self;
+end
+
+local function GetSparseIndex(x, y, z)
+	return y*900000000+x*30000+z;
+end
+
+-- convert from sparse index to block x,y,z
+-- @return x,y,z
+local function FromSparseIndex(index)
+	local x, y, z;
+	y = math.floor(index / (900000000));
+	index = index - y*900000000;
+	x = math.floor(index / (30000));
+	z = index - x*30000;
+	return x,y,z;
 end
 
 -- @param filename: agent xml or bmax or block template file. 
 -- @param cx, cy, cz: center position where the entities in the agent world are created into the real world. default to 0. 
 function AgentWorld:LoadFromAgentFile(filename, cx, cy, cz)
 	self:SetCenterPosition({cx or 0, cy or 0, cz or 0})
-	-- TODO:
+	local blocks = self.blocks;
+	local xmlRoot = ParaXML.LuaXML_ParseFile(filename);
+	if(xmlRoot) then
+		local root_node = commonlib.XPath.selectNode(xmlRoot, "/pe:blocktemplate");
+		if(root_node) then
+			local node = commonlib.XPath.selectNode(root_node, "/pe:blocks");
+			if(node and node[1]) then
+				local blocks_ = NPL.LoadTableFromString(node[1]);
+
+				if(blocks_ and #blocks_>=1) then
+					for _, b in ipairs(blocks_) do
+						local blockId = b[4];
+						blocks[GetSparseIndex(b[1], b[2], b[3])] = b;
+						if(blockId == block_types.names.CodeBlock) then
+							self.codeblocks[#(self.codeblocks) + 1] = b;
+						end
+					end
+				end
+			end
+		end
+	end
+end
+
+function AgentWorld:Clear()
 end
 
 -- run all code blocks in the agent world
